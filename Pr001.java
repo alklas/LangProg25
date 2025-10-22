c3ada4416e66bd65ba348b1b7caad138f57044b8
alexander <alklas@mail.ru>
2025-10-22 11:00:43 +0700

    Обобщённый класс с двумя параметрами типов

--- Pr001.java
+++ Pr001.java
@@ -14,27 +14,47 @@ class Gen<T> {
 		System.out.println("Тип T: " + ob.getClass().getName());
 	}
 }
+class TwoGen<T,V> { 
+	//Объявление переменных обобщённых типов
+	T ob1;
+	V ob2;
+	//Определение конструктора, в качестве параметров указываем объекты обобщённых типов
+	TwoGen(T o1, V o2) {
+		ob1 = o1;
+		ob2 = o2;
+	}
+	//Методы, возвращающие соответствующие объекты
+	T getOb1() {
+		return ob1;
+	}
+	V getOb2() {
+		return ob2;
+	}
+	//Методы, получающие информацию об имени типов
+	void showTypes() {
+		System.out.println("Тип T: " + ob1.getClass().getName());
+		System.out.println("Тип V: " + ob2.getClass().getName());
+	}
+}
 
 class Pr001 {
 	public static void main(String[] args) {
-		//Создаём ссылочную переменную на объект типа Integer
-		Gen<Integer> iOb;
-		//Присваиваем ссыку на новый объект
-		iOb = new Gen<Integer>(88);
-		//Выводим информацию о типе объекта
-		iOb.showType();
-		//Присваиваем целой переменной значение, хранящееся в объекте
-		int v = iOb.getOb();
+		Gen<String> strOb = new Gen<String>("Один обобщённый тип");
+		String str = strOb.getOb();
+		System.out.println("Значение обобщённого типа с одним параметром: "  + str);
+		//Создаём ссылочную переменную на объекты Integer и String
+		TwoGen<Integer, String> tgOb;
+		//Присваиваем ссыку на новые объекты
+		tgOb = new TwoGen<Integer, String>(88, "Обобщения");
+		//Выводим информацию о типе объектов
+		tgOb.showTypes();
+		//Присваиваем целой переменной значение, хранящееся в первом объекте
+		int v = tgOb.getOb1();
 		System.out.println("Значение: " + v);
 
 		System.out.println();
-		//Создаём ссылочную переменную на объект типа String и присваем ссылку на новый объект 
-		Gen<String> strOb = new Gen<String>("Строка обобщённого класса");
-
-		//Выводим информацию о типе объекта
-		strOb.showType();
-		//Присваиваем строковой переменной значение, хранящееся в объекте
-		String str = strOb.getOb();
+		//Присваиваем строковой переменной значение, хранящееся во втором объекте
+		str = tgOb.getOb2();
 		System.out.println("Значение в переменной str: " + str);
 	}
 }
